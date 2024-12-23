9b89d9dcdc48346eecf7eb7350ef11af5c45f374
Mahafuz <138840943+itsmahafuz@users.noreply.github.com>
2024-12-23 15:49:05 +0600

    Update 07. Form Largest Smallest Number From Digit.cpp

--- Normal solve/07. Form Largest Smallest Number From Digit.cpp
+++ Normal solve/07. Form Largest Smallest Number From Digit.cpp
@@ -1,90 +1,74 @@
-/* DATE : June 26th,2024
+/* DATE : December 23rd,2024
  MD MAHAFUZUR RAHMAN
- Roll:2110428176
- DEPARTMENT OF APPLIED MATHEMATICS
+ Roll: 2110428176
+ DEPARTMENT OF APPLIED MATHEMATICS,
+ University Of Rajshahi.
 LinkedIn : https://www.linkedin.com/in/md-mahafuzur-rahman-07b80b1b7
 GitHub : https://github.com/itsmahafuz
 */
 
-// Largest and smallest number that can be formed using digit of a given number.
+// Largest and smallest number that can be formed using digits of a given number
 
-#include <iostream>
-#include <cstring>
+#include<iostream>
+#include<string>
+#include<cmath>
 using namespace std;
-
-int numDigits(int num)
-{
-    int digits = 0;
-    if (num < 0)
-        digits = 1; // Remove this line if '-' doesn't count as a digit
-    while (num > 0)
-    {
-        num /= 10;
-        digits++;
-    }
-    return digits;
-}
 
 int main()
 {
-New:
-    int num;
-    int temp, i, j, x;
-
-    cout << "\nPlease Enter a positive integer number:\n";
-    cin >> num;
-
-    
-    int n = numDigits(num);
-    
-    string a = to_string(num);
-
-    /*For largest number we need to find out the descending order of 
-     the digits.*/
-    for (i = 0; i < n; i++)
+    char choice;  // Character to store user's choice for running the program again
+    do
     {
-        for (j = i + 1; j < n; j++)
+        string number;  // Variable to store the input number as a string
+        cout << "\nEnter a number:\n";
+        cin >> number;
+        int size = number.size();  // Get the size of the string using the inbuilt 'size()' function
+        
+        // Find the smallest number by sorting digits in ascending order
+        for(int i = 0; i < size; i++)
         {
-            if (a[i] < a[j])
+            for(int j = i + 1; j < size; j++)
             {
-                temp = a[i];
-                a[i] = a[j];
-                a[j] = temp;
+                if(number[i] > number[j])
+                {
+                    char temp = number[i];
+                    number[i] = number[j];
+                    number[j] = temp;
+                }
             }
         }
-    }
-
-    // Hence the descending order for the given entries will be...
-    cout << "\n The largest number that can be formed using the digits of the given number is : ";
-    for (i = 0; i < n; i++)
-        cout << a[i];
-    cout << endl;
-
-    // For smallest number we need to find out ascending order of the digit
-    // For ascending order
-    for (i = 0; i < n; i++)
-    {
-        for (j = i + 1; j < n; j++)
+        cout << "\nThe smallest number that can be formed using digits of the given number is: ";
+        for(int i = 0; i < size; i++)
         {
-            if (a[i] > a[j])
+            cout << number[i];
+        }
+        cout << endl;
+        
+        // Find the largest number by sorting digits in descending order
+        for(int i = 0; i < size; i++)
+        {
+            for(int j = i + 1; j < size; j++)
             {
-                temp = a[i];
-                a[i] = a[j];
-                a[j] = temp;
+                if(number[i] < number[j])
+                {
+                    char temp = number[i];
+                    number[i] = number[j];
+                    number[j] = temp;
+                }
             }
         }
-    }
-
-    // Hence the smallest number that can be formed ...
-    cout << "\n The smallest number that can be formed using the digits of the given number is : ";
-    for (i = 0; i < n; i++)
-        cout << a[i];
-    cout << endl;
-
-    cout << "\nDo you want to check with a new number?\n";
-    cout << "\nIf yes then press 1 otherwise press 0\n";
-    cin >> x;
-    if (x == 1)
-        goto New;
+        cout << "\nThe largest number that can be formed using digits of the given number is: ";
+        for(int i = 0; i < size; i++)
+        {
+            cout << number[i];
+        }
+        cout << endl;
+        
+        // Prompt the user to decide if they want to run the program again
+        cout << "\nIf you want to run the code again then press Y/y, otherwise press other characters: \n";
+        cin >> choice;
+    } while(tolower(choice) == 'y');  // Continue if the user enters 'y' or 'Y'
+    
     return 0;
 }
+
